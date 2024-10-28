d4a1a2bef130fb2702797f355188cebf6e479249
Jupiter Bold <sanadababy5@gmail.com>
2024-10-28 11:44:21 +0100

    ᴀʙᴏᴠᴇ ᴀʟʟ

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const express = require('express');
 const Config = require('./Config');
+const buttonHandler = require('./lib/buttonHandler');
+const plugins = require('./plugins');
 
 const client = new Client({ authStrategy: new LocalAuth() });
 const app = express();
@@ -12,6 +14,20 @@ client.on('qr', (qr) => {
 
 client.on('ready', () => {
     console.log('Client is ready!');
+});
+
+client.on('message', async message => {
+    if (message.body === '!menu') {
+        const buttons = [
+            { buttonId: 'id1', buttonText: { displayText: 'Joke' }, type: 1 },
+            { buttonId: 'id2', buttonText: { displayText: 'Info' }, type: 1 }
+        ];
+        const text = "Welcome to Pringles Md! Choose an option below:";
+        await buttonHandler.sendButtonMenu(client, message.from, text, buttons);
+    } else if (message.body === '!joke') {
+        const jokePlugin = require('./plugins/joke');
+        jokePlugin.execute(client, message);
+    }
 });
 
 client.initialize();
